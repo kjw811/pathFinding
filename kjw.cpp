0f36313eae97fcc1af40255656cc3173934b9a0b
joongwon_kim <joongwon_kim@tmax.co.kr>
2021-01-12 17:08:20 +0900

    [AStar 알고리즘 추가] 같은 케이스에 대해서 다익스트라와 에이스타를 비교해보기 위해 두 로직을 모두 추가해보았다.

--- kjw.cpp
+++ kjw.cpp
@@ -7,33 +7,61 @@
 #include <algorithm>
 #include <limits.h>
 #include <cmath>
+#include <chrono>
 
 using namespace std;
 
 vector<vector<char>> grid_init;
+int s_x;
+int s_y;
+int e_x;
+int e_y;
+
+struct Dijkstra
+{
+    Dijkstra(int x, int y, int d) : x(x), y(y), d(d) {}
+    int x;
+    int y;
+    int d;
+    bool operator<(Dijkstra& m) { return d > m.d; }
+};
+struct DijkstraComp
+{
+    bool operator()(Dijkstra& m1, Dijkstra& m2) { return m1 < m2; }
+};
+
+struct AStar
+{
+    AStar(int x, int y, int d, int e_x, int e_y) : x(x), y(y), d(d), e_x(e_x), e_y(e_y) {}
+    int x;
+    int y;
+    int d;
+    int e_x;
+    int e_y;
+    bool operator<(AStar& m)
+    {
+        int disToEnd1 = abs(x-e_x) + abs(y-e_y);
+        int disToEnd2 = abs(m.x-e_x) + abs(m.y-e_y);
+        if (d + disToEnd1 != m.d + disToEnd2) return d + disToEnd1 > m.d + disToEnd1;
+        else if (disToEnd1 != disToEnd2) return disToEnd1 > disToEnd2;
+        return d > m.d;
+    }
+};
+struct AStarComp
+{
+    bool operator()(AStar& m1, AStar& m2) { return m1 < m2; }
+};
 
 void initGrid()
 {
     char obstacle = 'x';
 
-    for (int i = 1; i < grid_init.size(); i++)
-        grid_init[i][0] = obstacle;
-    for (int i = 0; i < grid_init.size()-1; i++)
-        grid_init[i][5] = obstacle;
-    for (int i = 2; i <= 4; i++)
-        grid_init[2][i] = obstacle;
-    for (int i = 1; i <= 3; i++)
-        grid_init[4][i] = obstacle;
-    for (int i = 6; i <= 7; i++)
-        grid_init[i][2] = obstacle;
-    for (int i = 1; i <= 7; i++)
-        grid_init[i][10] = obstacle;
-    for (int i = 7; i <= 9; i++)
-        grid_init[4][i] = obstacle;
-    for (int i = 0; i <= 2; i++)
-        grid_init[i][8] = obstacle;
-    for (int i = 6; i <= 8; i++)
-        grid_init[6][i] = obstacle;
+    for (int i = 2; i <= 28; i++)
+        grid_init[i][14] = obstacle;
+    for (int i = 2; i <= 13; i++)
+        grid_init[13][i] = obstacle;
+    for (int i = 2; i <= 13; i++)
+        grid_init[15][i] = obstacle;
 }
 
 void printGrid(const vector<vector<char>>& grid)
@@ -53,6 +81,60 @@ void printGrid(const vector<vector<char>>& grid)
     }
 }
 
+void initStartEnd()
+{
+    printGrid(grid_init);
+    s_x = -1, s_y = -1, e_x = -1, e_y = -1;
+    while (1)
+    {
+        cout << "Start X (1~30) : "; cin >> s_x;
+        if (cin.fail())
+        {
+            cout << "Not a number" << endl;
+            cin.clear();
+            cin.ignore(256, '\n');
+            continue;
+        }
+        cout << "Start Y (1~30) : "; cin >> s_y;
+        if (cin.fail())
+        {
+            cout << "Not a number" << endl;
+            cin.clear();
+            cin.ignore(256, '\n');
+            continue;
+        }
+
+        if (s_x < 1 || s_x > 30 || s_y < 1 || s_y > 30 || grid_init[s_y-1][s_x-1] == 'x') cout << "Not available. Try again." << endl;
+        else break;
+    }
+    while (1)
+    {
+        cout << "End X (1~30) : "; cin >> e_x;
+        if (cin.fail())
+        {
+            cout << "Not a number" << endl;
+            cin.clear();
+            cin.ignore(256, '\n');
+            continue;
+        }
+        cout << "End Y (1~30) : "; cin >> e_y;
+        if (cin.fail())
+        {
+            cout << "Not a number" << endl;
+            cin.clear();
+            cin.ignore(256, '\n');
+            continue;
+        }
+        if (e_x < 1 || e_x > 30 || e_y < 1 || e_y > 30 || grid_init[e_y-1][e_x-1] == 'x') cout << "Not available. Try again." << endl;
+        else break;
+    }
+
+    cout << "======================================================" << endl;
+    cout << "Start  : " << s_x << ", " << s_y << " | End : " << e_x << ", " << e_y << endl;
+    cout << "======================================================" << endl;
+    s_x--, s_y--, e_x--, e_y--;
+}
+
 bool canMove(const vector<vector<char>>& grid, const vector<vector<bool>>& visited, int x, int y)
 {
     int rowSize = grid.size();
@@ -62,90 +144,31 @@ bool canMove(const vector<vector<char>>& grid, const vector<vector<bool>>& visit
     return true;
 }
 
-void findWay()
+void findWayDijkstra()
 {
     int rowSize = grid_init.size();
     int colSize = grid_init[0].size();
-    printGrid(grid_init);
 
     vector<vector<char>>grid = grid_init;
-
-    int s_x = -1, s_y = -1, e_x = -1, e_y = -1;
-    while (1)
-    {
-        cout << "Start X (1~12) : "; cin >> s_x;
-        if (cin.fail())
-        {
-            cout << "Not a number" << endl;
-            cin.clear();
-            cin.ignore(256, '\n');
-            continue;
-        }
-        cout << "Start Y (1~8) : "; cin >> s_y;
-        if (cin.fail())
-        {
-            cout << "Not a number" << endl;
-            cin.clear();
-            cin.ignore(256, '\n');
-            continue;
-        }
-
-        if (s_x < 1 || s_x > 12 || s_y < 1 || s_y > 8 || grid[s_y-1][s_x-1] == 'x') cout << "Not available. Try again." << endl;
-        else break;
-    }
-    while (1)
-    {
-        cout << "End X (1~12) : "; cin >> e_x;
-        if (cin.fail())
-        {
-            cout << "Not a number" << endl;
-            cin.clear();
-            cin.ignore(256, '\n');
-            continue;
-        }
-        cout << "End Y (1~8) : "; cin >> e_y;
-        if (cin.fail())
-        {
-            cout << "Not a number" << endl;
-            cin.clear();
-            cin.ignore(256, '\n');
-            continue;
-        }
-        if (e_x < 1 || e_x > 12 || e_y < 1 || e_y > 8 || grid[e_y-1][e_x-1] == 'x') cout << "Not available. Try again." << endl;
-        else break;
-    }
-
-    cout << "====================================" << endl;
-    cout << "Start  : " << s_x << ", " << s_y << " | End : " << e_x << ", " << e_y << endl;
-    s_x--, s_y--, e_x--, e_y--;
     grid[s_y][s_x] = 'S'; grid[e_y][e_x] = 'E';
+
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
 
     vector<vector<bool>> visited_init(rowSize, vector<bool>(colSize, false));
     vector<vector<bool>> visited = visited_init;
     visited[s_y][s_x] = true;
 
-    struct MoveInfo
-    {
-        MoveInfo(int x, int y, int d) : x(x), y(y), d(d) {}
-        int x;
-        int y;
-        int d;
-        bool operator<(MoveInfo& m) { return d > m.d; }
-    };
-    struct comp
-    {
-        bool operator()(MoveInfo& m1, MoveInfo& m2) { return m1 < m2; }
-    };
-
-    priority_queue<MoveInfo, vector<MoveInfo>, comp> pq;
-    pq.push(MoveInfo(s_x, s_y, 0));
+    priority_queue<Dijkstra, vector<Dijkstra>, DijkstraComp> pq;
+    pq.push(Dijkstra(s_x, s_y, 0));
 
     vector<vector<int>> dis(rowSize, vector<int>(colSize, INT_MAX));
     vector<vector<pair<int, int>>> fromInfo(rowSize, vector<pair<int, int>>(colSize, {-1, -1}));
     fromInfo[s_y][s_x] = {s_x, s_y};
 
+    int findCnt = 0;
     while (!pq.empty())
     {
+        findCnt++;
         int x = pq.top().x;
         int y = pq.top().y;
         int d = pq.top().d;
@@ -164,7 +187,7 @@ void findWay()
             {
                 fromInfo[y-1][x] = {x, y};
                 dis[y-1][x] = d+1;
-                pq.push(MoveInfo(x, y-1, d+1));
+                pq.push(Dijkstra(x, y-1, d+1));
             }
         }
         if (canMove(grid, visited, x, y+1))
@@ -173,7 +196,7 @@ void findWay()
             {
                 fromInfo[y+1][x] = {x, y};
                 dis[y+1][x] = d+1;
-                pq.push(MoveInfo(x, y+1, d+1));
+                pq.push(Dijkstra(x, y+1, d+1));
             }
         }
         if (canMove(grid, visited, x-1, y))
@@ -182,7 +205,7 @@ void findWay()
             {
                 fromInfo[y][x-1] = {x, y};
                 dis[y][x-1] = d+1;
-                pq.push(MoveInfo(x-1, y, d+1));
+                pq.push(Dijkstra(x-1, y, d+1));
             }
         }
         if (canMove(grid, visited, x+1, y))
@@ -191,7 +214,7 @@ void findWay()
             {
                 fromInfo[y][x+1] = {x, y};
                 dis[y][x+1] = d+1;
-                pq.push(MoveInfo(x+1, y, d+1));
+                pq.push(Dijkstra(x+1, y, d+1));
             }
         }
     }
@@ -209,12 +232,112 @@ void findWay()
     }
 
     printGrid(grid);
+
+    cout << "Move Count : " << findCnt << endl;
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout << ((std::chrono::duration<double>)(end - begin)).count() << " sec" << endl;
+    cout << "======================================================" << endl;
+}
+
+void findWayAStar()
+{
+    int rowSize = grid_init.size();
+    int colSize = grid_init[0].size();
+
+    vector<vector<char>>grid = grid_init;
+    grid[s_y][s_x] = 'S'; grid[e_y][e_x] = 'E';
+
+    chrono::steady_clock::time_point begin = chrono::steady_clock::now();
+
+    vector<vector<bool>> visited_init(rowSize, vector<bool>(colSize, false));
+    vector<vector<bool>> visited = visited_init;
+    visited[s_y][s_x] = true;
+
+    priority_queue<AStar, vector<AStar>, AStarComp> pq;
+    pq.push(AStar(s_x, s_y, 0, e_x, e_y));
+
+    vector<vector<int>> dis(rowSize, vector<int>(colSize, INT_MAX));
+    vector<vector<pair<int, int>>> fromInfo(rowSize, vector<pair<int, int>>(colSize, {-1, -1}));
+    fromInfo[s_y][s_x] = {s_x, s_y};
+
+    int findCnt = 0;
+    while (!pq.empty())
+    {
+        findCnt++;
+        int x = pq.top().x;
+        int y = pq.top().y;
+        int d = pq.top().d;
+        pq.pop();
+
+        if (x == e_x && y == e_y)
+        {
+            cout << "Shortest way length : " << d << endl;
+            break;
+        }
+
+        if (dis[y][x] < d) continue;
+        if (canMove(grid, visited, x, y-1))
+        {
+            if (d+1 < dis[y-1][x])
+            {
+                fromInfo[y-1][x] = {x, y};
+                dis[y-1][x] = d+1;
+                pq.push(AStar(x, y-1, d+1, e_x, e_y));
+            }
+        }
+        if (canMove(grid, visited, x, y+1))
+        {
+            if (d+1 < dis[y+1][x])
+            {
+                fromInfo[y+1][x] = {x, y};
+                dis[y+1][x] = d+1;
+                pq.push(AStar(x, y+1, d+1, e_x, e_y));
+            }
+        }
+        if (canMove(grid, visited, x-1, y))
+        {
+            if (d+1 < dis[y][x-1])
+            {
+                fromInfo[y][x-1] = {x, y};
+                dis[y][x-1] = d+1;
+                pq.push(AStar(x-1, y, d+1, e_x, e_y));
+            }
+        }
+        if (canMove(grid, visited, x+1, y))
+        {
+            if (d+1 < dis[y][x+1])
+            {
+                fromInfo[y][x+1] = {x, y};
+                dis[y][x+1] = d+1;
+                pq.push(AStar(x+1, y, d+1, e_x, e_y));
+            }
+        }
+    }
+
+    int from_x = e_x;
+    int from_y = e_y;
+    while (from_x != s_x ||from_y != s_y)
+    {
+        if (from_x != e_x || from_y != e_y)
+            grid[from_y][from_x] = '.';
+        int tmp_x = fromInfo[from_y][from_x].first;
+        int tmp_y = fromInfo[from_y][from_x].second;
+        from_x = tmp_x;
+        from_y = tmp_y;
+    }
+
+    printGrid(grid);
+
+    cout << "Move Count : " << findCnt << endl;
+    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    cout << ((std::chrono::duration<double>)(end - begin)).count() << " sec" << endl;
+    cout << "======================================================" << endl;
 }
 
 int main()
 {
-    int rowSize = 8;
-    int colSize = 12;
+    int rowSize = 30;
+    int colSize = 30;
     grid_init.resize(rowSize, vector<char>(colSize, ' '));
     initGrid();
 
@@ -226,7 +349,11 @@ int main()
         if (c == 'N' || c == 'n') return 0;
         else if (c == 'Y' || c == 'y')
         {
-            findWay();
+            initStartEnd();
+            cout << "Path Find by Dijkstar" << endl;
+            findWayDijkstra();
+            cout << "Path Find by AStar" << endl;
+            findWayAStar();
         }
     }
 
